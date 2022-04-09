0414f2e77d19e44dd593b5fd5c26880cc841bb7a
fatemah-alm <fatemah.alm@hotmail.com>
2022-04-09 17:41:28 +0300

    problem? SOLVED

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,2 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
+def helloworld(request):
+        return render(request, 'rest.html', {'msg':' Hello World!'})
--- task_02/urls.py
+++ task_02/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-
+from django.urls import path,include
+from restaurants import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',,name='hello-world'),
+    path('restaurants/',views.helloworld,name='hello-world'),
 ]
